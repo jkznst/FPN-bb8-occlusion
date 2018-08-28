5237642c57156a658fe11ca8cb8ebd1510a80722
zx <zx@buaa.com>
2018-08-28 15:41:17 +0800

    no freeze parameters

--- train.py
+++ train.py
@@ -147,7 +147,7 @@ if __name__ == '__main__':
             args.pretrained = 'model/resnet-50-0000.params'
         if not args.prefix:
             args.prefix = 'out/resnet50'
-        args.freeze_pattern = ['conv0', 'stage1', 'gamma', 'beta']  # used for fpn
+        args.freeze_pattern = []
     elif args.network == 'resnet101':
         if not args.pretrained:
             args.pretrained = 'model/resnet-101-0000.params'
